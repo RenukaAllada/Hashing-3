d2467aebfda10afcbe78388cd01d4ccdd0278a4b
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-03-30 09:35:35 -0500

    completed 2 exercises

--- sample.java
+++ sample.java
@@ -1,5 +1,24 @@
 class sample{
     /*****************PROBLEM-1***************/
+    //TC:0(N)
+//SC:0(N)
+    class Solution {
+        public List<String> findRepeatedDnaSequences(String s) {
+            if(s==null || s.length()<10){
+                return new ArrayList<>();
+            }
+            HashSet<String> set=new HashSet<>();
+            HashSet<String> result=new HashSet<>();
+            for(int i=0;i<=s.length()-10;i++){
+                String sub=s.substring(i,i+10);
+                if(set.contains(sub)){
+                    result.add(sub);
+                }
+                set.add(sub);
+            }
+            return new ArrayList<>(result);
+        }
+    }
     /*****************PROBLEM-2***************/
     //tc:0(kl+mn) m=length of userSongs, l=o.of songs,k=length of songsGenre ,n=no.of users
         //sc:0(k+n)
